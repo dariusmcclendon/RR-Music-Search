f2a8c16c650b4be6a9368f1751f7a308898f3d02
Darius <dariusmmcclendon@gmail.com>
2022-03-23 12:11:01 -0400

    Added code for React-Router-DOM assignment.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import {useEffect, useState } from 'react'
+import React, {useEffect, useState, Fragment } from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Gallery from './components/Gallery'
 import Searchbar from './components/Searchbar'
 import { DataContext } from './context/DataContext'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
 	let [search, setSearch] = useState('')
@@ -33,11 +36,26 @@ function App() {
 
 	return (
 		<div>
-			<Searchbar handleSearch = {handleSearch}/>
+			
 			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<Fragment>
+							<DataContext.Provider value={data}>
+								<Searchbar handleSearch = {handleSearch}/>
+								<Gallery data={data}/>
+							</DataContext.Provider>
+						</Fragment>
+							
+							
+						
+					}/>
+					<Route path="/album/:id" element={<AlbumView/>}/>
+					<Route path="/artist/:id" element={<ArtistView/>}/>
+				</Routes>
+			</Router>
+			
 		</div>
   	);
 }
